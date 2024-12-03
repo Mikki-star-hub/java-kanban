b831c1fbfa5f28bf3079a9e08a02eadc55660885
Mikki-star-hub <lelikov.1970@yandex.com>
2024-12-03 19:42:32 +0300

    Add files via upload

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -1,18 +1,12 @@
 package manager;
 
 import tasks.Task;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
     private static final int MAX_HISTORY_SIZE = 10; // Константа для максимального размера истории
     private final ArrayList<Task> history = new ArrayList<>();
-
-    public static int getMaxHistorySize() {
-        return MAX_HISTORY_SIZE;
-    }
 
     @Override
     public void add(Task task) {
